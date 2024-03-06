fcbc1454a31c639bdfc480010dc5d31a766d021d
VladyslavYakivets <aquartis97@gmail.com>
2024-03-06 21:03:50 +0200

    Сorrection

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,16 +2,16 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string()
-    .pattern(new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))
-    .required(),
-  phone: Joi.string()
-    .pattern(new RegExp(/^[\d()-]+$/))
-    .required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string().pattern(new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)),
-  phone: Joi.string().pattern(new RegExp(/^[\d()-]+$/)),
-});
+  email: Joi.string(),
+  phone: Joi.string(),
+})
+  .min(1)
+  .messages({
+    "object.min": "Body must have at least one field",
+  });
