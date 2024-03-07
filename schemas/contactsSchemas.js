5b48dbc92db2273a186a66776a8e0556f881e69d
VladyslavYakivets <aquartis97@gmail.com>
2024-03-07 22:21:21 +0200

    correct

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -9,9 +9,5 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
-  phone: Joi.string(),
-})
-  .min(1)
-  .messages({
-    "object.min": "Body must have at least one field",
-  });
+  phone: Joi.string().min(3),
+});
