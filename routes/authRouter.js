b9b248b3ba107fcd6cd80c646799a4201084b28a
VladyslavYakivets <aquartis97@gmail.com>
2024-03-23 13:10:33 +0200

    homework

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ import authController from "../controllers/authController.js";
 import validateBody from "../decorators/validateBody.js";
 import { userSignupSchema, userSigninSchema } from "../schemas/usersSchemas.js";
 import authenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
@@ -17,5 +18,12 @@ authRouter.post("/login", validateBody(userSigninSchema), authController.login);
 authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
 
 export default authRouter;
